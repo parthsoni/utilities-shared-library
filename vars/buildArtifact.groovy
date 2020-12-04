2d192ae7dc34860c2df223867b052db7dee90921
Parth Soni <parthsoni@live.in>
2020-12-04 22:14:57 +0530

    Update buildArtifact.groovy

--- vars/buildArtifact.groovy
+++ vars/buildArtifact.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def call(Map list) {
+def call(Map projectDetails) {
     
     sh "cd ${env.WORKSPACE}/${projectDetails.projectSourcePath} && npm install && npm run build" 
     sh "mkdir -p ${env.WORKSPACE}${projectDetails.buildDestinationPath} && cp -r ${projectDetails.buildSourcePath} ${projectDetails.buildDestinationPath}"
