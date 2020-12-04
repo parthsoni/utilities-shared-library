4f5a76945753da256636f955a6211f510a77f4db
Parth Soni <parthsoni@live.in>
2020-12-04 22:17:54 +0530

    Update buildContainerImage.groovy

--- vars/buildContainerImage.groovy
+++ vars/buildContainerImage.groovy
@@ -8,7 +8,7 @@ def call(Map projectDetails) {
         sh "docker build -t ${image_id} -f Dockerfile_${projectDetails.serviceName} ."
         withCredentials([string(credentialsId: 'DOCKER_HUB_CREDENTIALS', variable: 'DOCKER_HUB_CREDENTIALS')]) {
         new Docker().hubLogin(projectDetails.dockerRepo, DOCKER_HUB_CREDENTIALS);
-        sh "docker login -u ${projectDetails.dockerRepo} -p ${DOCKER_HUB_CREDENTIALS}"
+        //sh "docker login -u ${projectDetails.dockerRepo} -p ${DOCKER_HUB_CREDENTIALS}"
         sh "docker tag ${image_id} ${projectDetails.dockerRepo}/${image_id}"
         sh "docker push ${projectDetails.dockerRepo}/${image_id}"
         }
