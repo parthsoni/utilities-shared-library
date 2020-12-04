d8b81d1e1d42cc3b426ac553a84ff4447a3930bb
Parth Soni <parthsoni@live.in>
2020-12-04 20:18:07 +0530

    Update pullProject.groovy

--- vars/pullProject.groovy
+++ vars/pullProject.groovy
@@ -7,9 +7,9 @@ def call(Map projectDetails) {
         def image_id = projectDetails.image_id
         sh "cd ${env.WORKSPACE}/eskinfront && npm install && npm run build"
         sh "mkdir -p ${env.WORKSPACE}${projectDetails.dest} && cp -r ${projectDetails.src} ${projectDetails.dest}"
-        sh "docker build -t ${image_id} -f Dockerfile_nginx ."
+        sh "docker build -t ${image_id} -f Dockerfile_${projectDetails.serviceName} ."
         withCredentials([string(credentialsId: 'DOCKER_HUB_CREDENTIALS', variable: 'DOCKER_HUB_CREDENTIALS')]) {
-        hubLogin("parthsoni", ${DOCKER_HUB_CREDENTIALS});
+        hubLogin(${projectDetails.dockerRepo}, ${DOCKER_HUB_CREDENTIALS});
         //sh "docker login -u parthsoni -p ${DOCKER_HUB_CREDENTIALS}"
         sh "docker tag ${image_id} ${projectDetails.dockerRepo}/${image_id}"
         sh "docker push ${projectDetails.dockerRepo}/${image_id}"
