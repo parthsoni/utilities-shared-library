fd929a3fde0f162d0e33b37086ddb752fe7076fa
Parth Soni <parthsoni@live.in>
2020-12-04 20:09:49 +0530

    Update pullProject.groovy

--- vars/pullProject.groovy
+++ vars/pullProject.groovy
@@ -9,7 +9,7 @@ def call(Map projectDetails) {
         sh "mkdir -p ${env.WORKSPACE}${projectDetails.dest} && cp -r ${projectDetails.src} ${projectDetails.dest}"
         sh "docker build -t ${image_id} -f Dockerfile_nginx ."
         withCredentials([string(credentialsId: 'DOCKER_HUB_CREDENTIALS', variable: 'DOCKER_HUB_CREDENTIALS')]) {
-        new docker.hubLogin("parthsoni", ${DOCKER_HUB_CREDENTIALS});
+        new Docker.hubLogin("parthsoni", ${DOCKER_HUB_CREDENTIALS});
         //sh "docker login -u parthsoni -p ${DOCKER_HUB_CREDENTIALS}"
         sh "docker tag ${image_id} parthsoni/${image_id}"
         sh "docker push parthsoni/${image_id}"
