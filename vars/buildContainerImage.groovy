3d004c34970451c91bd10bbbb17dafd167c24d92
Parth Soni <parthsoni@live.in>
2020-12-04 22:01:10 +0530

    Update and rename pullProject.groovy to buildContainerImage.groovy

--- vars/buildContainerImage.groovy
+++ vars/buildContainerImage.groovy
@@ -5,8 +5,6 @@ import org.docker.*
 def call(Map projectDetails) {
         echo " ${env.WORKSPACE}"
         def image_id = projectDetails.image_id
-        sh "cd ${env.WORKSPACE}/eskinfront && npm install && npm run build"
-        sh "mkdir -p ${env.WORKSPACE}${projectDetails.dest} && cp -r ${projectDetails.src} ${projectDetails.dest}"
         sh "docker build -t ${image_id} -f Dockerfile_${projectDetails.serviceName} ."
         withCredentials([string(credentialsId: 'DOCKER_HUB_CREDENTIALS', variable: 'DOCKER_HUB_CREDENTIALS')]) {
         new Docker().hubLogin(projectDetails.dockerRepo, DOCKER_HUB_CREDENTIALS);
@@ -14,6 +12,5 @@ def call(Map projectDetails) {
         sh "docker tag ${image_id} ${projectDetails.dockerRepo}/${image_id}"
         sh "docker push ${projectDetails.dockerRepo}/${image_id}"
         }
-        
 }
 
