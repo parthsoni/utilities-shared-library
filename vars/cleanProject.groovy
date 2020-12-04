3c197406a60b21a90b35f0001fbc7afc75317b35
Parth Soni <parthsoni@live.in>
2020-12-04 22:10:48 +0530

    Update cleanProject.groovy

--- vars/cleanProject.groovy
+++ vars/cleanProject.groovy
@@ -1,4 +1,7 @@
 def call(Map projectDetails)
 {
  echo "Cleaning up and preparing for project :- ${projectDetails.projectName}"
+ 
+ echo "Pull source code from Repository"
+ git branch: '${projectDetails.projecBranch}', credentialsId: '${projectDetails.gitCredential}', url: '${projectDetails.gitPath}'
 }
