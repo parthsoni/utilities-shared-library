b62b23d53dea1161f901b82abbbcc1fa04743c75
Parth Soni <parthsoni@live.in>
2020-12-04 19:31:57 +0530

    Update and rename pullProject to pullProject.groovy

---
+++ vars/pullProject.groovy
@@ -0,0 +1,14 @@
+
+def call(Map projectDetails) {
+
+        def NGINX_BUILD_PATH = "${env.WORKSPACE}"
+        def image_id = ${projectDetails.image_id}
+        sh "cd ${env.WORKSPACE}/eskinfront && npm install && npm run build"
+        sh "mkdir -p ${env.WORKSPACE}${list.dest} && cp -r ${list.src} ${list.dest}"
+        sh "docker build -t ${image_id} -f Dockerfile_nginx ."
+        withCredentials([string(credentialsId: 'DOCKER_HUB_CREDENTIALS', variable: 'DOCKER_HUB_CREDENTIALS')]) {
+        sh "docker login -u parthsoni -p ${DOCKER_HUB_CREDENTIALS}"
+        sh "docker tag ${image_id} parthsoni/${image_id}"
+        sh "docker push parthsoni/${image_id}"
+        }
+}
