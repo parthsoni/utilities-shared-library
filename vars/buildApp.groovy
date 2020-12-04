fe7254864d25040b761ab7c9cab95ec383127092
Parth Soni <parthsoni@live.in>
2020-12-04 14:07:49 +0530

    Update buildApp.groovy

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -2,22 +2,20 @@
 
 def call(Map list) {
     
-    
-    println list.dump()
-
+   
     node {
         
         stage('Clean workspace and prepare') { // for display purposes
         // Get some code from a GitHub repository
-        git branch: list.branch, credentialsId: 'eSkinDoctor_Repo', url: 'https://github.com/pNAIA/eskindoctor'
+            git branch: ${list.branch}, credentialsId: 'eSkinDoctor_Repo', url: 'https://github.com/pNAIA/eskindoctor'
     }
         stage('Build artifacts and docker image') {
         def USER_HOME = "/home/ubuntu/"
         def NGINX_BUILD_PATH = "${env.WORKSPACE}"
-        def image_id = list.image_id
+            def image_id = ${list.image_id}
         //sh "cp -r ${USER_HOME}docker_config ${env.WORKSPACE}"
         sh "cd ${env.WORKSPACE}/eskinfront && npm install && npm run build"
-        sh "mkdir -p ${env.WORKSPACE}/eskinfront/patient/ && cp -r list.src list.dest"
+            sh "mkdir -p ${env.WORKSPACE}/eskinfront/patient/ && cp -r ${list.src} ${list.dest}"
         
         dir("${env.WORKSPACE}") {
         sh "pwd"
