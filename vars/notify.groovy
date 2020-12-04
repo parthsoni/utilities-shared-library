c6150a43f910a54b62d73b199b8a3258881d1e19
Ubuntu <parthsoni@live.in>
2020-12-04 06:07:35 +0000

    [Added :src and vars ]

--- vars/notify.groovy
+++ vars/notify.groovy
@@ -1,11 +1,27 @@
 import org.utility.Utility
-def call(Map config=[:]) {
+def call(Map config=[:], String buildResult){
     if (config.type == "slack") {
         echo Utility.SLACK_MESSAGE
         echo config.message
+        slackNotitfy()
     } else {
         echo Utility.EMAIL_MESSAGE
         echo config.message
     }
 }
 
+
+def slackNotitfy(String buildResult) {
+  if ( buildResult == "SUCCESS" ) {
+    slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful"
+  }
+  else if( buildResult == "FAILURE" ) { 
+    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
+  }
+  else if( buildResult == "UNSTABLE" ) { 
+    slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable"
+  }
+  else {
+    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear"	
+  }
+}
