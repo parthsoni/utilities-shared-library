82ca10459bef55975f01517e80a19890f9e5aa9e
Parth Soni <parthsoni@live.in>
2020-12-04 19:44:34 +0530

    Update pullProject.groovy

--- vars/pullProject.groovy
+++ vars/pullProject.groovy
@@ -1,15 +1,18 @@
 #!/usr/bin/env groovy
 
 def call(Map projectDetails) {
-
-        def NGINX_BUILD_PATH = "${WORKSPACE}"
-        def image_id = ${projectDetails.image_id}
-        sh "cd ${env.WORKSPACE}/eskinfront && npm install && npm run build"
-        sh "mkdir -p ${env.WORKSPACE}${list.dest} && cp -r ${list.src} ${list.dest}"
-        sh "docker build -t ${image_id} -f Dockerfile_nginx ."
+        buildImage(${projectDetails})
         withCredentials([string(credentialsId: 'DOCKER_HUB_CREDENTIALS', variable: 'DOCKER_HUB_CREDENTIALS')]) {
         sh "docker login -u parthsoni -p ${DOCKER_HUB_CREDENTIALS}"
         sh "docker tag ${image_id} parthsoni/${image_id}"
         sh "docker push parthsoni/${image_id}"
         }
+}
+
+def buildImage(projectDetails)
+{
+        def image_id = ${projectDetails.image_id}
+        sh "cd ${env.WORKSPACE}/eskinfront && npm install && npm run build"
+        sh "mkdir -p ${env.WORKSPACE}${list.dest} && cp -r ${list.src} ${list.dest}"
+        sh "docker build -t ${image_id} -f Dockerfile_nginx ."   
 }
