6c3e01b07945c99df9813fa969d7bcc14a55fdd3
Parth Soni <parthsoni@live.in>
2020-12-04 22:06:47 +0530

    Update buildArtifact.groovy

--- vars/buildArtifact.groovy
+++ vars/buildArtifact.groovy
@@ -2,40 +2,6 @@
 
 def call(Map list) {
     
-   
-    node {
-        
-        stage('Clean workspace and prepare') { // for display purposes
-        // Get some code from a GitHub repository
-            git branch: ${list.branch}, credentialsId: 'eSkinDoctor_Repo', url: 'https://github.com/pNAIA/eskindoctor'
-    }
-        stage('Build artifacts and docker image') {
-        def USER_HOME = "/home/ubuntu/"
-        def NGINX_BUILD_PATH = "${env.WORKSPACE}"
-            def image_id = ${list.image_id}
-        //sh "cp -r ${USER_HOME}docker_config ${env.WORKSPACE}"
-        sh "cd ${env.WORKSPACE}/eskinfront && npm install && npm run build"
-            sh "mkdir -p ${env.WORKSPACE}/eskinfront/patient/ && cp -r ${list.src} ${list.dest}"
-        
-        dir("${env.WORKSPACE}") {
-        sh "pwd"
-        sh "whoami"
-            sh "docker build -t ${image_id} -f Dockerfile_nginx ."
-        withCredentials([string(credentialsId: 'DOCKER_HUB_CREDENTIALS', variable: 'DOCKER_HUB_CREDENTIALS')]) {
-    // some block
-            sh "docker login -u parthsoni -p ${DOCKER_HUB_CREDENTIALS}"
-        }
-        sh "docker tag ${image_id} parthsoni/${image_id}"
-        sh "docker push parthsoni/${image_id}"
-        }
-        
-        //sh "cp -r ${USER_HOME}docker_config ${env.WORKSPACE}"
-        //sh "mkdir -p ${env.WORKSPACE}/eskinfront/patient/ && cp -r eskinfront/dist/* ${env.WORKSPACE}/eskinfront/patient/"
-        //&& npm install && npm run build'
-        
-        
-    }
-
-        
-    }
+    sh "cd ${env.WORKSPACE}/${projectDetails.projectSourcePath} && npm install && npm run build" 
+    sh "mkdir -p ${env.WORKSPACE}${projectDetails.buildDestinationPath} && cp -r ${projectDetails.buildSourcePath} ${projectDetails.buildDestinationPath}"
 }
