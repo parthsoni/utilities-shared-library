a2e689b2a727fcd6a58e141cae559473e6088d29
Parth Soni <parthsoni@live.in>
2021-11-14 11:51:13 +0530

    Update buildArtifact.groovy

--- vars/buildArtifact.groovy
+++ vars/buildArtifact.groovy
@@ -6,7 +6,6 @@ def call(Map projectDetails) {
             sh "cd ${projectDetails.projectSourcePath} && npm install -g npm-install-peers && npm run build" 
 
     }else {
-            sh "sudo chmod 777 -R /usr/local/lib/node_modules"
             sh "npm install -g npm-install-peers && npm run build" 
     }
     sh "mkdir -p ${env.WORKSPACE}/${projectDetails.buildDestinationPath} && cp -r ${projectDetails.buildSourcePath} ${projectDetails.buildDestinationPath}"
